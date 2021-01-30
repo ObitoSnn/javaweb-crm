94f589c9b9d4f96ae318324b5e20c7d2fe32e879
ObitoSnn <obitosnn@163.com>
2021-01-30 19:19:42 +0800

    建表sql文件

---
+++ crm-数据字典.sql
@@ -0,0 +1,27 @@
+drop table if exists tbl_dic_type;
+
+drop table if exists tbl_dic_value;
+
+/*==============================================================*/
+/* Table: tbl_dic_type                                          */
+/*==============================================================*/
+create table tbl_dic_type
+(
+    code                 varchar(255) not null comment '编码是主键，不能为空，不能含有中文。',
+    name                 varchar(255),
+    description          varchar(255),
+    primary key (code)
+);
+
+/*==============================================================*/
+/* Table: tbl_dic_value                                         */
+/*==============================================================*/
+create table tbl_dic_value
+(
+    id                   char(32) not null comment '主键，采用UUID',
+    value                varchar(255) comment '不能为空，并且要求同一个字典类型下字典值不能重复，具有唯一性。',
+    text                 varchar(255) comment '可以为空',
+    orderNo              varchar(255) comment '可以为空，但不为空的时候，要求必须是正整数',
+    typeCode             varchar(255) comment '外键',
+    primary key (id)
+);
