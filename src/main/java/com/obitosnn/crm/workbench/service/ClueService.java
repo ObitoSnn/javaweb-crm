f5c6341c7e9b3013ff1102df7b3efc589b5322d6
ObitoSnn <obitosnn@163.com>
2021-02-01 14:13:55 +0800

    搭建“线索”后端结构(service)

---
+++ src/main/java/com/obitosnn/crm/workbench/service/ClueService.java
@@ -0,0 +1,8 @@
+package com.obitosnn.crm.workbench.service;
+
+/**
+ * @Author ObitoSnn
+ * @Date 2021/1/31 14:11
+ */
+public interface ClueService {
+}
---
+++ src/main/java/com/obitosnn/crm/workbench/service/impl/ClueServiceImpl.java
@@ -0,0 +1,14 @@
+package com.obitosnn.crm.workbench.service.impl;
+
+import com.obitosnn.crm.workbench.dao.ClueDao;
+import com.obitosnn.crm.workbench.service.ClueService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Author ObitoSnn
+ * @Date 2021/1/31 14:11
+ */
+public class ClueServiceImpl implements ClueService {
+    @Autowired
+    private ClueDao clueDao;
+}
