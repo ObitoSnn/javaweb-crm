b2b7844a48e15716a877432427a9b2a0170d0958
ObitoSnn <obitosnn@163.com>
2021-03-01 23:22:17 +0800

    完成部门管理主页面分页功能

---
+++ src/main/java/com/obitosnn/crm/settings/dao/DeptDao.java
@@ -0,0 +1,18 @@
+package com.obitosnn.crm.settings.dao;
+
+import com.obitosnn.crm.settings.domain.Dept;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ObitoSnn
+ * @Date 2021/3/1 21:46
+ */
+public interface DeptDao {
+
+    List<Dept> selectDeptListForPageVo(Map<String, Object> map);
+
+    Long selectDeptTotalForPageVo(Map<String, Object> map);
+
+}
